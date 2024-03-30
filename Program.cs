95aa04a54ed4c817a232d1b2e86ae4528710bba9
Ignac Brychta <orbitrek1411@gmail.com>
2024-03-30 21:16:23 +0100

    crlf round 2

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ internal class Program
 	{
 		string input;
 #if DEBUG
-		input = File.ReadAllText("../../../input data/warehouse map large.txt");
+		input = File.ReadAllText("../../../input data/warehouse map large CRLF.txt");
 #else
 		input = Console.In.ReadToEnd();
 #endif
